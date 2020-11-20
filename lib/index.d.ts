62ae170007e0e33a20a211c0916d319b7939b2ec
eightfeet <bajiao000000@163.com>
2020-11-20 13:48:34 +0800

    update

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,5 @@
-import Message from './Message';
+import Message, { Parameters } from './Message';
 import { createInlineStyles } from './inlineStyle';
 export default Message;
 export { createInlineStyles };
+export declare type MessageParameters = Parameters;
